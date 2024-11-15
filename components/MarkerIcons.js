46a2a257b90df74d044e37ffb99f52460c584536
Miguel <miguel.mendez@alu.ucm.cl>
2024-11-15 00:45:16 -0300

    last comit 60%
    agregar categoria Laboratorio, ubicacion de 1 laboratorio y icono del marcador

--- components/MarkerIcons.js
+++ components/MarkerIcons.js
@@ -1,9 +1,11 @@
 import ComidaIcon from '../assets/ComidaIcon.png'
 import FacultadIcon from '../assets/FacultadIcon.png'
 import EdificioIcon from '../assets/EdificioIcon.png'
+import LaboratorioICon from '../assets/LaboratorioICon.png'
 
 export const CategoryIcon = {
     Comida: ComidaIcon,
     Facultad: FacultadIcon,
     Edificio: EdificioIcon,
+    Laboratorio: LaboratorioICon,
 }
